483f8b5ac5a386a94b52ff8eb4f063bd03c28960
Ashab Ahmed <ashabahmed7@gmail.com>
2020-07-26 18:54:29 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -29,19 +29,19 @@ def monopoly_with_third_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
-  somethingelse = monopoly_with_second_tier
+  something_else = monopoly_with_second_tier
   
-  somethingelse[:railroads][:rent_in_dollars][:one_piece_owned] = 25
-  somethingelse[:railroads][:rent_in_dollars][:two_pieces_owned] = 50
-  somethingelse[:railroads][:rent_in_dollars][:three_pieces_owned] = 100
-  somethingelse[:railroads][:rent_in_dollars][:four_pieces_owned] = 200
+  something_else[:railroads][:rent_in_dollars][:one_piece_owned] = 25
+  something_else[:railroads][:rent_in_dollars][:two_pieces_owned] = 50
+  something_else[:railroads][:rent_in_dollars][:three_pieces_owned] = 100
+  something_else[:railroads][:rent_in_dollars][:four_pieces_owned] = 200
 
-  somethingelse[:railroads][:names][:reading_railroad] = {}
-  somethingelse[:railroads][:names][:pennsylvania_railroad] = {}
-  somethingelse[:railroads][:names][:b_and_o_railroad] = {}
-  somethingelse[:railroads][:names][:shortline_railroad] = {}
+  something_else[:railroads][:names][:reading_railroad] = {}
+  something_else[:railroads][:names][:pennsylvania_railroad] = {}
+  something_else[:railroads][:names][:b_and_o_railroad] = {}
+  something_else[:railroads][:names][:shortline_railroad] = {}
   
-somethingelse
+something_else
 end
 
 def monopoly_with_fourth_tier
@@ -49,5 +49,9 @@ def monopoly_with_fourth_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+ another_something = monopoly_with_third_tier
  
+ another_something[:railroads][:names][:reading_railroad][:mortgage_value] = 100
+
+another_something
 end
