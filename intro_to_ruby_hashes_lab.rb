c1c06f3792f856a774ab0801ef5b2a5d6de2c743
Ashab Ahmed <ashabahmed7@gmail.com>
2020-07-26 18:45:32 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -38,7 +38,7 @@ def monopoly_with_third_tier
 
   somethingelse[:railroads][:names][:reading_railroad] = {}
   somethingelse[:railroads][:names][:pennsylvania_railroad] = {}
-  somethingelse[:railroads][:names][:reading_railroad] = {}
+  somethingelse[:railroads][:names][:b_and_o_railroad] = {}
   somethingelse[:railroads][:names][:reading_railroad] = {}
   
 somethingelse
