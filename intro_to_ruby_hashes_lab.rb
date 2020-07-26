6b2a835ce4d6d3a56135a0a1e811a8bf2e3fd674
Ashab Ahmed <ashabahmed7@gmail.com>
2020-07-26 18:05:22 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -14,7 +14,7 @@ def monopoly_with_second_tier
   # complexity.
    bash_hash[:railroads][:pieces] = 4
    bash_hash[:railroads][:rent_in_dollars] = {}
-   bash_hash[:railroads[:names] = {}
+   bash_hash[:railroads][:names] = {}
 
 end
 
