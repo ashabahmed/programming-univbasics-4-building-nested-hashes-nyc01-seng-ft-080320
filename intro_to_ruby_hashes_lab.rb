6ab734d7389f8a2f679588ff134c35ab84da2386
Ashab Ahmed <ashabahmed7@gmail.com>
2020-07-26 18:08:30 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,7 +13,6 @@ def monopoly_with_second_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   something = base_hash
-  binding.pry
    something[:railroads][:pieces] = 4
 
 end
