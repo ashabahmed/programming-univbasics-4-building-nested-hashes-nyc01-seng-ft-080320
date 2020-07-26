3eab58a70306a397c6fdb177e9efdb334c0fd08b
Ashab Ahmed <ashabahmed7@gmail.com>
2020-07-26 18:04:56 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,7 +12,9 @@ def monopoly_with_second_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
-   bash_hash[:railroads] = 
+   bash_hash[:railroads][:pieces] = 4
+   bash_hash[:railroads][:rent_in_dollars] = {}
+   bash_hash[:railroads[:names] = {}
 
 end
 
