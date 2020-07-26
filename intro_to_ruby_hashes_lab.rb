ad9b872f3fd9db83c4cee7596b100ac7196fa4e7
Ashab Ahmed <ashabahmed7@gmail.com>
2020-07-26 18:17:17 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,6 +13,7 @@ def monopoly_with_second_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   something = base_hash
+  somethinelse = base_hash
   something[:railroads][:pieces] = "4"
   return something
 end
