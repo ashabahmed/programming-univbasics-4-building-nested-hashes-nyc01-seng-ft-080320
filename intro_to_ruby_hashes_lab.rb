32e7c51fe4ba42d2d55b99501d78e6afe411d8e6
Ashab Ahmed <ashabahmed7@gmail.com>
2020-07-26 18:10:46 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -14,7 +14,7 @@ def monopoly_with_second_tier
   # complexity.
   something = base_hash
    something[:railroads][:pieces] = "4"
-  return somethign
+  return something
 end
 
 def monopoly_with_third_tier
